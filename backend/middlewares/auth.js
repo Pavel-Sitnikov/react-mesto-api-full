45c7377279ff408d81b51dc41b65ea0d2b8bdbb5
Pavel Sitnikov <sitnikovpa@gmail.com>
2022-10-01 03:26:07 +0500

    feat: провка auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     return next(new UnauthorizedError('Ошибка авторизации'));
   }
